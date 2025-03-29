9754fda895713797b5312a63b183a0674bc69da8
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-28 20:36:55 -0600

    Etiqueta de Select

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const App = () => {
-    const [value, setValue] = useState({ normal: 'por defecto', texto: ''})
+    const [value, setValue] = useState({ normal: 'por defecto', texto: '', select: '' })
     const handleChange = (e) => {
         setValue((state) => ({
             ...state,
@@ -14,6 +14,14 @@ const App = () => {
             {value.length < 5 ? <span>Longitud minima de 5</span> : null}
             <input type="text" name="normal" value={value.normal} onChange={handleChange}/>
             <textarea name="texto" onChange={handleChange} value={value.texto}/>
+
+            <select value={value.select} name="select" onChange={handleChange}>
+                <option value=''>-- Seleccione --</option>
+                <option value='gatofeliz'>Gato feliz</option>
+                <option value='gatitofeliz'>Gatito feliz</option>
+                <option value='gatitotriste'>Gatito triste</option>
+                <option value='alonso'>Alonso</option>
+            </select>
         </div>
     )
 }
